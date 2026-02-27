48735786a6dd43e9e3b1fa9a54aa8f076c5fb87a
Dane Lim <144203132+danelim1203@users.noreply.github.com>
2026-02-27 21:53:13 +0700

    add script db and product field

---
+++ src/main/Product.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.Date;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private int qty;
+    private Date date;
+}
---
+++ src/script/stock product.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tb_product (
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(100) UNIQUE NOT NULL,
+                            unit_price DOUBLE PRECISION NOT NULL CHECK (unit_price >= 0),
+                            stock_qty INT NOT NULL CHECK (stock_qty >= 0),
+                            import_date DATE
+);
+
+INSERT INTO tb_product (name, unit_price, stock_qty)
+VALUES ('Computer', 350, 4),
+       ('Keyboard', 25, 23),
+       ('USB C', 10, 3),
+       ('Monitor', 20, 23),
+       ('Laptop', 5, 11),
+       ('Chacher', 15, 23),
+       ('Nokai', 50, 4),
+       ('HDMI', 11, 7),
+       ('Printer', 9, 82),
+       ('Phone Case', 17, 17);
+
+TRUNCATE TABLE tb_product RESTART IDENTITY;
